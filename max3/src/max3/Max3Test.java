d15739fd024e0d1f6f8969a5fe68c9f5fad04dc7
RyanDeKoninck <RyanDaKing3@yahoo.com>
2020-02-18 18:01:43 +0100

    Initial commit

---
+++ max3/src/max3/Max3Test.java
@@ -0,0 +1,14 @@
+package max3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Max3Test {
+
+	@Test
+	void test() {
+		assert Max3.max3(10, 20, 30) == 30;
+	}
+
+}
